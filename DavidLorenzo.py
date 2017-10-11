ab834c798196e027a778db58e43bbbc218d082a5
EnforcerZhukov <davidl.eco@gmail.com>
2017-10-11 14:23:48 +0200

    add DavidLorenzo.py

---
+++ DavidLorenzo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+
+delay = 0.025
+
+print("Huehuehue :V")
+
+for i in range(420):
+	print( "Moar SPAM!!! >:V #{}".format(i+1) )
+	sleep(delay)
+
+print("Adiós! D:")
