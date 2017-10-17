7f2fb8c61422c9733c0d2d3ce2cd35f8c2e6f38d
David Lorenzo <EnforcerZhukov@users.noreply.github.com>
2017-10-17 09:05:59 +0200

    Update DavidLorenzo.py

--- DavidLorenzo.py
+++ DavidLorenzo.py
@@ -6,10 +6,8 @@ from time import sleep
 delay = 0.042
 N=420
 
-print("Hi there!")
-
 for i in range(1,N+1):
-	print( "Moar SPAM!!! >:V #{}".format(i+1) )
+	print( "Moar SPAM!!! :D #{}".format(i+1) )
 	sleep(delay)
 
 print("Adiós! D:")
