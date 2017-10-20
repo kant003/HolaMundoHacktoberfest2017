f52b8b6f59cf71e65c7326e715b3f9c8aff6f795
David Lorenzo <EnforcerZhukov@users.noreply.github.com>
2017-10-20 09:23:02 +0200

    Add files via upload

---
+++ GTA5_Armor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GTA;
+
+namespace GTAV_ArmorRecovery
+{
+    public class ArmorRecovery : Script
+    {
+        private int nativeArmor;
+        private int prevArmor;
+        private Ped prevPlayer;
+        private const bool debugActivated = true;
+
+        public ArmorRecovery()
+        {
+            Tick += OnTick;
+            Interval = 100;
+        }
+
+        private void debug(string s)
+        {
+            GTA.UI.ShowSubtitle(s);
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Ped player = Game.Player.Character;
+            int armor = player.Armor;
+
+            bool hasArmor = false;
+            if (player.Armor > 0) hasArmor = true;
+            if (player != prevPlayer) {
+                debug("Player character has changed!");
+            }
+            else if (armor < prevArmor) {
+                debug("Armor decreased! (from " + prevArmor.ToString() + " to " + armor.ToString() + ")");
+            }
+            else if (armor > prevArmor) {
+                debug("Armor increased! (from " + prevArmor.ToString() + " to " + armor.ToString() + ")");
+            }
+
+            prevArmor = armor;
+            prevPlayer = player;
+        }
+
+    }
+}
