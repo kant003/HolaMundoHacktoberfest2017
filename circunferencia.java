c2f339cd77a28dd7c13674c0fc526613c84ed2e0
fabird9 <fabird9@gmail.com>
2017-10-23 09:58:05 +0200

    circunferencia

---
+++ circunferencia.java
@@ -0,0 +1,13 @@
+//radio*3*2*5
+import java.util.Scanner;
+public class circunferencia {
+public static void main(String[]args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Intruduzca valor del radio: ");
+	double radio;
+	radio=sc.nextDouble();
+	System.out.println("La longitud de la circunferencia es: " + 2*Math.PI*radio);
+	sc.close();;;;
+}
+}
